6363f8523e9a7c982ad6c4fda2be607ae31c59f1
Monte <36746765+theflaggship@users.noreply.github.com>
2021-08-13 10:49:09 -0700

    image seeds added

--- backend/seed_data/Addresses.py
+++ backend/seed_data/Addresses.py
@@ -1,7 +1,7 @@
 # Monte
 # 1 { address_line_one: "942 Arrow Hwy STE #6", address_line_two: null, city: "Covina", state: "CA", postal_code: 91722, country: "United States" }
-# 2 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
-# 3 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
+# 2 { address_line_one: "347 N San Gabriel Ave", address_line_two: null, city: "Azusa", state: "CA", postal_code: "91702", country: "United States" }
+# 3 { address_line_one: "498 S Rosemead Blvd", address_line_two: null, city: "Pasadena", state: "CA", postal_code: "91107", country: "United States" }
 
 # Sunny
 # 4 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
@@ -12,8 +12,7 @@
 # 7 { address_line_one: "789 9th Avenue", address_line_two: null, city: "New York", state: "NY", postal_code: 10019, country: "United States" }
 # 8 { address_line_one: "607 Trade Street NW", address_line_two: Floor 1, city: "Winston-Salem", state: "NC", postal_code: 27101, country: "United States" }
 # 9 { address_line_one: "871 W 4th Street", address_line_two: null, city: "Winston-Salem", state: "NC", postal_code: 27101, country: "United States" }
-# update address id
-# 9 { address_line_one: "111 E 7th Street", address_line_two: null, city: "New York", state: "NY", postal_code: 10009, country: "United States" }
+
 # Jonathan
 # 10 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
 # 11 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
--- backend/seed_data/Restaurants.py
+++ backend/seed_data/Restaurants.py
@@ -6,9 +6,9 @@
 
 
 # Monte
-# 1 { name: "Crazy Rock'N Sushi", phone_number: "(626) 334-2430", address_id: 1, business_id: 1, description: "text", price_range: 2, cuisine_id: 1, created_at: new Date(), updated_at: new Date() },
-# 2 { name: "name", phone_number: "number", address_id: 2, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
-# 3 { name: "name", phone_number: "number", address_id: 3, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() }
+# 1 { name: "Crazy Rock'N Sushi", phone_number: "(626) 334-2430", address_id: 1, business_id: 1, description: "It is our great pleasure to serve you with the highest quality food and excellent service at the best prices. On the frontier of Japanese fusion restaurants, we are proud of our unique and wonderful cuisine which is created from the finest ingredients. We promise you the freshest seafood, meat and produce. The fun & friendly atmosphere of the sushi bar and the beautiful presentation of each dish with their remarkable and distinctive flavors have made us and experience that inspires our guests to return again and again.", price_range: 2, cuisine_id: 1, created_at: new Date(), updated_at: new Date() },
+# 2 { name: "Canyon City BBQ", phone_number: "(626) 815-4227", address_id: 2, business_id: "1", description: "Our restaurant sits at the foothills of the San Gabriel Mountains.  We have been told that our restaurant looks and feels like the ones down south.  Whether it’s the tri-tip, brisket, ribs, burnt ends, or pulled pork sandwiches, the meat is cooked to perfection, and the baked beans, coleslaw, or potato salad make perfectly delicious side dishes.", price_range: "2", cuisine_id: "2", created_at: new Date(), updated_at: new Date() },
+# 3 { name: "President Thai", phone_number: "(626) 578-9814", address_id: 3, business_id: "1", description: "Established in 1994, President Thai’s mission is to offer to the community, the authenticity in Thai food, culture, and atmosphere. Rebuilt to become the first traditional Thai style architecture restaurant in Los Angeles with the interior finely decorated with ornate Thai art form, and soothed with traditional music. Customers will find themselves being taken back in time to experience the traditional ways of Thai’s culture. We are ready to greet you in Thai, Sawadee ka!", price_range: "2", cuisine_id: "4", created_at: new Date(), updated_at: new Date() }
 
 # Sunny
 # 4 { name: "name", phone_number: "number", address_id: 4, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
@@ -17,9 +17,9 @@
 
 # Pierre
 # TODO Add cuisine type ids.
-# 8 { name: "The Meatball Shop", phone_number: "(212) 230-5860", address_id: 7, business_id: 3, description: "Buzzy outfit serving a range house-ground meatballs, sauces & sides plus ice cream sandwiches.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
-# 9 { name: "Sweet Potatoes", phone_number: "(336) 727-4844", address_id: 8, business_id: 3, description: "Chicken & waffles, mac 'n' cheese & banana pudding round out the menu at this soul-food staple.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
-# 10 { name: "The Old Fourth Street Filling Station", phone_number: "(336) 724-7600", address_id: 9, business_id: 3, description: "Trendy upscale New American eatery & bar with a fireplace & covered outdoor terrace.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() }
+# 7 { name: "The Meatball Shop", phone_number: "(212) 230-5860", address_id: 7, business_id: 3, description: "Buzzy outfit serving a range house-ground meatballs, sauces & sides plus ice cream sandwiches.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
+# 8 { name: "Sweet Potatoes", phone_number: "(336) 727-4844", address_id: 8, business_id: 3, description: "Chicken & waffles, mac 'n' cheese & banana pudding round out the menu at this soul-food staple.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
+# 9 { name: "The Old Fourth Street Filling Station", phone_number: "(336) 724-7600", address_id: 9, business_id: 3, description: "Trendy upscale New American eatery & bar with a fireplace & covered outdoor terrace.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() }
 # TODO change id, address-id, cuisine_id
 
 
