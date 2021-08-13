2a5c572ae685e54fba1b436b6fbbae349480bb9f
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-13 12:31:18 +0200

    pierre seed data

--- backend/seed_data/Addresses.py
+++ backend/seed_data/Addresses.py
@@ -9,10 +9,11 @@
 # 6 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
 
 # Pierre
-# 7 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
-# 8 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
-# 9 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
-
+# 7 { address_line_one: "789 9th Avenue", address_line_two: null, city: "New York", state: "NY", postal_code: 10019, country: "United States" }
+# 8 { address_line_one: "607 Trade Street NW", address_line_two: Floor 1, city: "Winston-Salem", state: "NC", postal_code: 27101, country: "United States" }
+# 9 { address_line_one: "871 W 4th Street", address_line_two: null, city: "Winston-Salem", state: "NC", postal_code: 27101, country: "United States" }
+# update address id
+# 9 { address_line_one: "111 E 7th Street", address_line_two: null, city: "New York", state: "NY", postal_code: 10009, country: "United States" }
 # Jonathan
 # 10 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
 # 11 { address_line_one: "address", address_line_two: null, city: "city", state: "ST", postal_code: "#", country: "United States" }
--- backend/seed_data/Restaurants.py
+++ backend/seed_data/Restaurants.py
@@ -1,4 +1,4 @@
-#Price Range:
+# Price Range:
 # 1 : under $10 per plate
 # 2 : $11-$20 per plate
 # 3 : $21-$30 per plate
@@ -16,9 +16,12 @@
 # 6 { name: "name", phone_number: "number", address_id: 6, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() }
 
 # Pierre
-# 8 { name: "name", phone_number: "number", address_id: 7, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
-# 9 { name: "name", phone_number: "number", address_id: 8, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
-# 10 { name: "name", phone_number: "number", address_id: 9, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() }
+# TODO Add cuisine type ids.
+# 8 { name: "The Meatball Shop", phone_number: "(212) 230-5860", address_id: 7, business_id: 3, description: "Buzzy outfit serving a range house-ground meatballs, sauces & sides plus ice cream sandwiches.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
+# 9 { name: "Sweet Potatoes", phone_number: "(336) 727-4844", address_id: 8, business_id: 3, description: "Chicken & waffles, mac 'n' cheese & banana pudding round out the menu at this soul-food staple.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
+# 10 { name: "The Old Fourth Street Filling Station", phone_number: "(336) 724-7600", address_id: 9, business_id: 3, description: "Trendy upscale New American eatery & bar with a fireplace & covered outdoor terrace.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() }
+# TODO change id, address-id, cuisine_id
+# 10 { name: "Ladybird", phone_number: "(833) 328-4588", address_id: 9, business_id: 3, description: "Creative vegetarian small plates & cocktails in an embellished setting with plush blankets.", price_range: 2, cuisine_id: "#", created_at: new Date(), updated_at: new Date() }
 
 # Jonathan
 # 10 { name: "name", phone_number: "number", address_id: 10, business_id: "#", description: "text", price_range: "1-5", cuisine_id: "#", created_at: new Date(), updated_at: new Date() },
