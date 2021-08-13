876b4b737c756d5db587e5dee13cf7ff94d1f632
Monte <36746765+theflaggship@users.noreply.github.com>
2021-08-13 15:08:39 -0700

    reformat seeds without create update date

--- backend/seed_data/Businesses.py
+++ backend/seed_data/Businesses.py
@@ -1,4 +1,4 @@
-# 1 {email: "monte@business.com", business_name: "TheFlaggShip Group", first_name: "Monte", last_name: "Flagg", hashed_password: "$2a$10$TJJnlZd.EHsV95T9a/PtMeIiucJigTc.ExS7Rb.4P4eHFXXjfk1bG", created_at: new Date(), updated_at: new Date()}
-# 2 {email: "sunny@business.com", business_name: "SunnyCorp", first_name: "Sunny", last_name: "Mallick", hashed_password: "$2a$10$TJJnlZd.EHsV95T9a/PtMeIiucJigTc.ExS7Rb.4P4eHFXXjfk1bG", created_at: new Date(), updated_at: new Date()}
-# 3 {email: "pierre@business.com", business_name: "PMG", first_name: "Pierre", last_name: "Guilbault", hashed_password: "$2a$10$d.EmV3dONS6OAvliUwm2Recy81dELXDtQ4K9ucgO8JB5ip.5HOIum", created_at: new Date(), updated_at: new Date()}
-# 4 {email: "jonathan@business.com", business_name: "QPS", first_name: "Jonathan", last_name: "Borja", hashed_password: "$2a$10$OsJzm1ueYiTX48ywpYm4QuXHsdrbH8U3DpjN6pTfvideJ1dLYLAYW", created_at: new Date(), updated_at: new Date()}
+# 1 {email: "monte@business.com", business_name: "TheFlaggShip Group", first_name: "Monte", last_name: "Flagg", password: "password"}
+# 2 {email: "sunny@business.com", business_name: "SunnyCorp", first_name: "Sunny", last_name: "Mallick", password: "password"}
+# 3 {email: "pierre@business.com", business_name: "PMG", first_name: "Pierre", last_name: "Guilbault", password: "password"}
+# 4 {email: "jonathan@business.com", business_name: "QPS", first_name: "Jonathan", last_name: "Borja", password: "password"}
