ed53d55418e14db1b38d4dffdd48cd840e6a1900
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-14 01:02:17 +0200

    seed data for cuisines and users model for users

--- backend/seed_data/Cuisines.py
+++ backend/seed_data/Cuisines.py
@@ -5,6 +5,17 @@
 #     bobbie = User(
 #         username='bobbie', email='bobbie@aa.io', password='password')
 
+sushi = Cuisine(type="Sushi")
+bbq = Cuisine(type="BBQ")
+italian = Cuisine(type="Italian")
+thai = Cuisine(type="Thai")
+vegan = Cuisine(type="Vegan")
+french = Cuisine(type="French")
+vietnamese = Cuisine(type="Vietnamese")
+korean = Cuisine(type="Korean")
+american = Cuisine(type="American")
+southern = Cuisine(type="Southern")
+indian = Cuisine(type="Indian")
 
 # { type: "Sushi"},
 # { type: "BBQ"},
