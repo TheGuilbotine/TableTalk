de14a2a45d074d89ed554f5a3d73dfeb047bd9b1
Monte <36746765+theflaggship@users.noreply.github.com>
2021-08-13 18:15:03 -0700

    seed formatting and favicon

--- backend/seed_data/Businesses.py
+++ backend/seed_data/Businesses.py
@@ -1,10 +1,3 @@
-# demo = User(
-#         username='Demo', email='demo@aa.io', password='password')
-#     marnie = User(
-#         username='marnie', email='marnie@aa.io', password='password')
-#     bobbie = User(
-#         username='bobbie', email='bobbie@aa.io', password='password')
-
 # monte = Business(
 #     email="monte@business.com", business_name="TheFlaggShip Group", first_name="Monte", last_name="Flagg", password="password")
 # sunny = Business(
@@ -13,9 +6,3 @@
 #     email="pierre@business.com", business_name="PMG", first_name="Pierre", last_name="Guilbault", password="password")
 # jonathan = Business(
 #     email="jonathan@business.com", business_name="QPS", first_name="Jonathan", last_name="Borja", password="password")
-
-
-# 1 {email: "monte@business.com", business_name: "TheFlaggShip Group", first_name: "Monte", last_name: "Flagg", password: "password"}
-# 2 {email: "sunny@business.com", business_name: "SunnyCorp", first_name: "Sunny", last_name: "Mallick", password: "password"}
-# 3 {email: "pierre@business.com", business_name: "PMG", first_name: "Pierre", last_name: "Guilbault", password: "password"}
-# 4 {email: "jonathan@business.com", business_name: "QPS", first_name: "Jonathan", last_name: "Borja", password: "password"}
