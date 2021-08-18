a66579e61e1545372c1d91abc1f8a7645226fe72
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-18 23:30:20 +0200

    Login for business works, everything works

--- app/api/routes/auth_routes.py
+++ app/api/routes/auth_routes.py
@@ -82,13 +82,14 @@ def business_login():
     Logs a business in
     """
     form = BusinessLoginForm()
+    print(form['password'].data)
     # Get the csrf_token from the request cookie and put it into the
     # form manually to validate_on_submit can be used
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
         business = Business.query.filter(
-            User.email == form.data['email']).first()
+            Business.email == form.data['email']).first()
         login_user(business)
         return business.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
