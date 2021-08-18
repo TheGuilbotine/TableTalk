a3108266cd3b93235e7524c1d33534b3399a7d33
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-18 23:08:54 +0200

    a lot of code

--- app/api/routes/restaurant_routes.py
+++ app/api/routes/restaurant_routes.py
@@ -1,5 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, request
+from flask_request_params import bind_request_params
 from app.models.restaurant import Restaurant
+from flask_login import login_required
+from app.forms.restaurant_form import RestaurantForm
+from app.models.db import db
+
 
 restaurant_routes = Blueprint('restaurants', __name__)
 
@@ -14,3 +19,36 @@ def restaurants():
 def restaurant(id):
     restaurant = Restaurant.query.get(id)
     return restaurant.to_dict()
+
+
+@restaurant_routes.route('/', methods=['POST'])
+@login_required
+def create_restaurant():
+    form = Restaurant()
+    if form.validate_on_submit():
+        data = RestaurantForm()
+        form.populate_obj(data)
+        db.session.add(data)
+        db.session.commit()
+        return redirect("/f'{data.id}'")
+    errors = form.errors
+    print(errors)
+
+
+@restaurant_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_restaurant(id):
+    business = request.args.get('business')
+    print(business)
+    Restaurant.query.get(id).delete()
+    db.session.commit()
+    # TODO which business f'{business.id}
+    return redirect("/business'")
+
+
+@restaurant_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_restaurant(id):
+    Restaurant.query.get(id).update()
+    db.session.commit()
+    return redirect("/<int:id>")
