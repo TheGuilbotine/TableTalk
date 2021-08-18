cc699398dda33af9e353c1ab6672b8521510fc8e
Monte <36746765+theflaggship@users.noreply.github.com>
2021-08-17 20:40:01 -0700

    business page css start

--- react-app/src/components/BusinessHomePage/index.js
+++ react-app/src/components/BusinessHomePage/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import BusinessSignUpForm from '../BusinessSignUpForm'
+import './BusinessHomePage.css'
 
 function BusinessHomePage() {
     return (
-        <BusinessSignUpForm />
+        <div className='business-home-page'>
+            <div className="business-page-left">
+             <h1>Sign up with us today because...</h1>
+             <h3>1: Fill more seats! lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkjfasdfj</h3>
+             <h3>2: Earn more money! lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkjfasdfj</h3>
+             <h3>3: Curate new social experiences! lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkjfasdfj</h3>
+            </div>
+            <div classNme='business-page-right'>
+                <BusinessSignUpForm />
+            </div>
+        </div>
     )
 }
 
