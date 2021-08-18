2853edf33fdd987b9d5e074cdeeb63e0eab15431
jborja-one <jborja.one@gmail.com>
2021-08-18 11:16:07 -0600

    modal css

--- react-app/src/components/BusinessLoginFormModal/index.js
+++ react-app/src/components/BusinessLoginFormModal/index.js
@@ -3,21 +3,27 @@ import { Modal } from '../../context/Modal';
 import BusinessLoginForm from './BusinessLoginFormModal';
 
 function BusinessLoginFormModal() {
-  const [showModal, setShowModal] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 
-  return (
-    <>
-      <div className="business-login-button" onClick={() => setShowModal(true)}>Login Business</div>
-      {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <div className="login-title-container">
-            <h3 className="login-title">Login to TableTalk Business</h3>
-          </div>
-          <BusinessLoginForm />
-        </Modal>
-      )}
-    </>
-  )
+	return (
+		<>
+			<div
+				className='business-login-button'
+				onClick={() => setShowModal(true)}>
+				Login Business
+			</div>
+			{showModal && (
+				<Modal onClose={() => setShowModal(false)}>
+					<div className='login-title-container'>
+						<h3 className='login-title'>
+							Login to TableTalk Business
+						</h3>
+					</div>
+					<BusinessLoginForm />
+				</Modal>
+			)}
+		</>
+	);
 }
 
 export default BusinessLoginFormModal;
