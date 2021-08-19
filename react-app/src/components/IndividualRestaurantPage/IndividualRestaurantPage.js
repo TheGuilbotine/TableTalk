c7420cca6b63e17af8aca87b0de58b68075139b5
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-18 23:08:17 -0600

    working on individual restaurant page and query

---
+++ react-app/src/components/IndividualRestaurantPage/IndividualRestaurantPage.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
+import { getOneRestaurant } from '../../store/restaurants';
+import './IndividualRestaurantPage.css'
+
+function IndividualRestaurant() {
+    const dispatch = useDispatch()
+    const { id } = useParams()
+
+    const restaurant = useSelector((state) => state.restaurants[id])
+
+    useEffect(() => {
+        dispatch(getOneRestaurant(id))
+    }, [dispatch, id])
+
+    return (
+        <>
+            <div className='restaurant-details'>
+                <h1>{restaurant?.restaurant_name}</h1>
+                <p>{restaurant?.description}</p>
+                <p>{restaurant?.phone_number}</p>
+                <p>{restaurant?.cuisine.type}</p>
+            </div>
+        </>
+    )
+}
+
+export default IndividualRestaurant
