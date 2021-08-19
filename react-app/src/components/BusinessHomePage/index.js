7cea0cfa6914248ff6f71a0c89f9d3e92ab4b18b
jborja-one <jborja.one@gmail.com>
2021-08-18 21:44:01 -0600

    update

--- react-app/src/components/BusinessHomePage/index.js
+++ react-app/src/components/BusinessHomePage/index.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import BusinessSignUpForm from '../BusinessSignUpForm'
-import './BusinessHomePage.css'
+import React from 'react';
+import BusinessSignUpForm from '../BusinessSignUpForm';
+import './BusinessHomePage.css';
 
 function BusinessHomePage() {
-    return (
-        <div className='business-home-page'>
-            <div className="business-page-left">
-             <h1>Sign up with us today because...</h1>
-             <h3>1: Fill more seats! lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkjfasdfj</h3>
-             <h3>2: Earn more money! lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkjfasdfj</h3>
-             <h3>3: Curate new social experiences! lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkjfasdfj</h3>
-            </div>
-            <div classNme='business-page-right'>
-                <BusinessSignUpForm />
-            </div>
-        </div>
-    )
+	return (
+		<div className='business-home-page'>
+			<div className='business-page-left'>
+				<h1>Sign up with us today because...</h1>
+				<h3>
+					1: Fill more seats!
+					lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkj
+				</h3>
+				<h3>
+					2: Earn more money!
+					lajsdlkfjasdlfkjasldkjfasldkjfasdljkfaslkj
+				</h3>
+				<h3>
+					3: Curate new social experiences!
+					lajsdlkfjasdlfkjasldkjfasldk
+				</h3>
+			</div>
+			<div classNme='business-page-right'>
+				<BusinessSignUpForm />
+			</div>
+		</div>
+	);
 }
 
-export default BusinessHomePage
+export default BusinessHomePage;
