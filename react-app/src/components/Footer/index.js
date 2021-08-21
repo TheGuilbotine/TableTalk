2a0a4d356da8930ae23c8b9dfafa1cc40edfc26c
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-21 16:59:53 +0200

    footer styling

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -2,54 +2,124 @@ import './Footer.css'
 
 export default function Footer() {
     return (
-        <footer className='footer'>
-            <p className='footer__designed-by'>Designed By:</p>
-            <div className='footer__name-link__container'>
-                <div className='footer__name-link'>
-                    <h3 className='footer__name'>Jonathan Borja</h3>
-                    <div className='footer__icons'>
-                        <a className='footer__link' href='https://github.com/jborja-one' target="_blank">
-                            <i class="fab fa-github-square"></i>
-                        </a>
-                        <a className='footer__link' href='https://www.linkedin.com/in/jonathan-borja-1a9959172/' target="_blank">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </div>
-                </div>
-                <div className='footer__name-link'>
-                    <h3 className='footer__name'>Monte Flagg</h3>
-                    <div className='footer__icons'>
-                        <a className='footer__link' href='https://github.com/theflaggship' target="_blank">
-                            <i className="fab fa-github-square"></i>
-                        </a>
-                        <a className='footer__link' href='https://www.linkedin.com/in/montgomeryflagg/' target="_blank">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </div>
-                </div>
-                <div className='footer__name-link'>
-                    <h3 className='footer__name'>Pierre Guilbault</h3>
-                    <div className='footer__icons'>
-                        <a className='footer__link' href='https://github.com/TheGuilbotine' target="_blank">
-                            <i className="fab fa-github-square"></i>
-                        </a>
-                        <a className='footer__link' href='https://www.linkedin.com/in/pierre-guilbault-30020549/' target="_blank">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </div>
-                </div>
-                <div className='footer__name-link'>
-                    <h3 className='footer__name'>Sunny Mallick</h3>
-                    <div className='footer__icons'>
-                        <a className='footer__link' href='https://github.com/sunnymallick' target="_blank">
-                            <i className="fab fa-github-square"></i>
-                        </a>
-                        <a className='footer__link' href='https://www.linkedin.com/in/sunny-mallick-896a33169/' target="_blank">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
+        <footer className="footer">
+            <div className="footer__container">
+                <div className="footer-info__container">
+                    <div className="footer-info">
+                        <div className="footer-columns__container">
+                            <div className="column__container">
+                                <div className="column__title">Future of TableTalk</div>
+                                <ul>
+                                    <li>Share your table</li>
+                                    <li>Review restaurants</li>
+                                    <li>Review your experience</li>
+                                    <li>Rewards</li>
+                                    <li>Chat</li>
+                                </ul>
+                            </div>
+                            <div className="column__container">
+                                <div className="column__title">TableTalk</div>
+                                <ul>
+                                    <li className="footer-link__container">
+                                        <a className='footer__link' href='</li>' target="_blank">
+                                        About TableTalk <i class="fab fa-github-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='footer__link' href='https://github.com/jborja-one' target="_blank">
+                                            About Jonathan <i class="fab fa-github-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='footer__link' href='https://github.com/theflaggship' target="_blank">
+                                            About Monte <i className="fab fa-github-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='footer__link' href='https://github.com/TheGuilbotine' target="_blank">
+                                            About Pierre <i className="fab fa-github-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='footer__link' href='https://github.com/sunnymallick' target="_blank">
+                                            About Sunny <i className="fab fa-github-square"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="column__container">
+                                <div className="column__title">Contact us on LinkedIn</div>
+                                <ul>
+                                    <li>JONATHAN</li>
+                                    <li>MONTE</li>
+                                    <li>PIERRE</li>
+                                    <li>SUNNY</li>
+                                </ul>
+                            </div>
+                            <div className="column__container">
+                                <div className="column__title">Our Projects</div>
+                                <ul>
+                                    <li>JONATHAN</li>
+                                    <li>MONTE</li>
+                                    <li>PIERRE</li>
+                                    <li>SUNNY</li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </footer>
+        // <footer className='footer'>
+        //     <p className='footer__designed-by'>Designed By:</p>
+        //     <div className='footer__name-link__container'>
+        //         <div className='footer__name-link'>
+        //             <h3 className='footer__name'>Jonathan Borja</h3>
+        //             <div className='footer__icons'>
+        //                 <a className='footer__link' href='https://github.com/jborja-one' target="_blank">
+        //                     <i class="fab fa-github-square"></i>
+        //                 </a>
+        //
+        //                 <a className='footer__link' href='https://www.linkedin.com/in/jonathan-borja-1a9959172/' target="_blank">
+        //                     <i className="fab fa-linkedin"></i>
+        //                 </a>
+        //             </div>
+        //         </div>
+        //         <div className='footer__name-link'>
+        //             <h3 className='footer__name'>Monte Flagg</h3>
+        //             <div className='footer__icons'>
+        //                 <a className='footer__link' href='https://github.com/theflaggship' target="_blank">
+        //                     <i className="fab fa-github-square"></i>
+        //                 </a>
+        //                 <a className='footer__link' href='https://www.linkedin.com/in/montgomeryflagg/' target="_blank">
+        //                     <i className="fab fa-linkedin"></i>
+        //                 </a>
+        //             </div>
+        //         </div>
+        //         <div className='footer__name-link'>
+        //             <h3 className='footer__name'>Pierre Guilbault</h3>
+        //             <div className='footer__icons'>
+                        // <a className='footer__link' href='https://github.com/TheGuilbotine' target="_blank">
+                        //     <i className="fab fa-github-square"></i>
+                        // </a>
+        //                 <a className='footer__link' href='https://www.linkedin.com/in/pierre-guilbault-30020549/' target="_blank">
+        //                     <i className="fab fa-linkedin"></i>
+        //                 </a>
+        //             </div>
+        //         </div>
+        //         <div className='footer__name-link'>
+        //             <h3 className='footer__name'>Sunny Mallick</h3>
+        //             <div className='footer__icons'>
+                        // <a className='footer__link' href='https://github.com/sunnymallick' target="_blank">
+                        //     <i className="fab fa-github-square"></i>
+                        // </a>
+        //                 <a className='footer__link' href='https://www.linkedin.com/in/sunny-mallick-896a33169/' target="_blank">
+        //                     <i className="fab fa-linkedin"></i>
+        //                 </a>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </footer>
     )
 }
