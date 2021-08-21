975fac8fa91f0649fe484a7a2c2c28fefa4d3257
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-21 17:03:57 +0200

    linkedin links

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -50,10 +50,26 @@ export default function Footer() {
                             <div className="column__container">
                                 <div className="column__title">Contact us on LinkedIn</div>
                                 <ul>
-                                    <li>JONATHAN</li>
-                                    <li>MONTE</li>
-                                    <li>PIERRE</li>
-                                    <li>SUNNY</li>
+                                    <li>
+                                        <a className='footer__link' href='https://www.linkedin.com/in/jonathan-borja-1a9959172/' target="_blank">
+                                            Jonathan Borja<i className="fab fa-linkedin"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='footer__link' href='https://www.linkedin.com/in/montgomeryflagg/' target="_blank">
+                                            Monte Flagg<i className="fab fa-linkedin"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='footer__link' href='https://www.linkedin.com/in/pierre-guilbault-30020549/' target="_blank">
+                                            Pierre Guilbault<i className="fab fa-linkedin"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='footer__link' href='https://www.linkedin.com/in/sunny-mallick-896a33169/' target="_blank">
+                                            Sunny Mallick<i className="fab fa-linkedin"></i>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                             <div className="column__container">
@@ -81,9 +97,9 @@ export default function Footer() {
         //                     <i class="fab fa-github-square"></i>
         //                 </a>
         //
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/jonathan-borja-1a9959172/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
+                        // <a className='footer__link' href='https://www.linkedin.com/in/jonathan-borja-1a9959172/' target="_blank">
+                        //     <i className="fab fa-linkedin"></i>
+                        // </a>
         //             </div>
         //         </div>
         //         <div className='footer__name-link'>
@@ -92,9 +108,9 @@ export default function Footer() {
         //                 <a className='footer__link' href='https://github.com/theflaggship' target="_blank">
         //                     <i className="fab fa-github-square"></i>
         //                 </a>
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/montgomeryflagg/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
+                        // <a className='footer__link' href='https://www.linkedin.com/in/montgomeryflagg/' target="_blank">
+                        //     <i className="fab fa-linkedin"></i>
+                        // </a>
         //             </div>
         //         </div>
         //         <div className='footer__name-link'>
@@ -103,9 +119,9 @@ export default function Footer() {
                         // <a className='footer__link' href='https://github.com/TheGuilbotine' target="_blank">
                         //     <i className="fab fa-github-square"></i>
                         // </a>
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/pierre-guilbault-30020549/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
+                        // <a className='footer__link' href='https://www.linkedin.com/in/pierre-guilbault-30020549/' target="_blank">
+                        //     <i className="fab fa-linkedin"></i>
+                        // </a>
         //             </div>
         //         </div>
         //         <div className='footer__name-link'>
@@ -114,9 +130,9 @@ export default function Footer() {
                         // <a className='footer__link' href='https://github.com/sunnymallick' target="_blank">
                         //     <i className="fab fa-github-square"></i>
                         // </a>
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/sunny-mallick-896a33169/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
+                        // <a className='footer__link' href='https://www.linkedin.com/in/sunny-mallick-896a33169/' target="_blank">
+                        //     <i className="fab fa-linkedin"></i>
+                        // </a>
         //             </div>
         //         </div>
         //     </div>
