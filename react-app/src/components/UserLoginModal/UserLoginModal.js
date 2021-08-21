24b91d35b97324eee877e5804db0ab90cbfce606
Monte <36746765+theflaggship@users.noreply.github.com>
2021-08-21 11:39:27 -0700

    modal css work

--- react-app/src/components/UserLoginModal/UserLoginModal.js
+++ react-app/src/components/UserLoginModal/UserLoginModal.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { userLogin } from '../../store/session';
+import * as sessionActions from '../../store/session'
 
-const UserLoginForm = () => {
+const UserLoginForm = ({setShowModal}) => {
 	const [errors, setErrors] = useState([]);
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
@@ -13,10 +14,22 @@ const UserLoginForm = () => {
 	const onLogin = async (e) => {
 		e.preventDefault();
 		const data = await dispatch(userLogin(email, password));
+
 		if (data) {
 			setErrors(data);
 		}
+		if (user) {
+			setShowModal(false)
+		}
 	};
+
+	const demoLogin = async (e) => {
+		e.preventDefault()
+    const user = await dispatch(userLogin('demo@user.com','password'))
+		if (user) {
+			setShowModal(false)
+		}
+  }
 
 	const updateEmail = (e) => {
 		setEmail(e.target.value);
@@ -39,9 +52,6 @@ const UserLoginForm = () => {
 					))}
 				</div>
 				<div className='form-label__container'>
-					<label className='form-label' htmlFor='email'>
-						Email
-					</label>
 					<input
 						className='form-input'
 						name='email'
@@ -52,9 +62,6 @@ const UserLoginForm = () => {
 					/>
 				</div>
 				<div className='form-label__container'>
-					<label className='form-label' htmlFor='password'>
-						Password
-					</label>
 					<input
 						className='form-input'
 						name='password'
@@ -65,9 +72,12 @@ const UserLoginForm = () => {
 					/>
 				</div>
 				<div className='form-button__container'>
-					<button className='form-button' type='submit'>
+					<button className='user-login-button' type='submit'>
 						Login
 					</button>
+				</div>
+				<div className="demo-login-container">
+					<p className="demo-text">To demo TableTalk, <a className="demo-click-here" onClick={demoLogin}>Click Here</a></p>
 				</div>
 			</form>
 		</div>
