1f77ccd3b3b57751cbca9b0fe0a9d2b6216c82c5
Pierre Guilbault <guilbaultpj@gmail.com>
2021-09-24 08:52:17 -0400

    remove superfulous text

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -22,7 +22,7 @@ export default function Footer() {
                                 </ul>
                             </div>
                             <div className="column__container">
-                                <div className="column__title">TableTalk</div>
+                                <div className="column__title">GitHub</div>
                                 <ul>
                                     <li className="footer-link__container">
                                         <a className='footer__link' href='https://github.com/TheGuilbotine/TableTalk' target="_blank">
@@ -112,12 +112,11 @@ export default function Footer() {
                                 </ul>
                             </div>
                             <div className="column__container">
-                                <div className="column__title">Our Portfolios</div>
+                                <div className="column__title">TableTalk Contributors</div>
                                 <ul>
-                                    <li>JONATHAN</li>
                                     <li>
-                                    <a className='footer__link' href='https://spademagick.herokuapp.com/' target="_blank">
-                                            SpadeMagick
+                                    <a className='footer__link' href='https://jborja-one.github.io/' target="_blank">
+                                            Jonathan Borja
                                         </a>
                                     </li>
                                     <li className="asterisk">*</li>
@@ -149,55 +148,5 @@ export default function Footer() {
                 </div>
             </div>
         </footer>
-        // <footer className='footer'>
-        //     <p className='footer__designed-by'>Designed By:</p>
-        //     <div className='footer__name-link__container'>
-        //         <div className='footer__name-link'>
-        //             <h3 className='footer__name'>Jonathan Borja</h3>
-        //             <div className='footer__icons'>
-        //                 <a className='footer__link' href='https://github.com/jborja-one' target="_blank">
-        //                     <i class="fab fa-github-square"></i>
-        //                 </a>
-
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/jonathan-borja-1a9959172/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
-        //             </div>
-        //         </div>
-        //         <div className='footer__name-link'>
-        //             <h3 className='footer__name'>Monte Flagg</h3>
-        //             <div className='footer__icons'>
-        //                 <a className='footer__link' href='https://github.com/theflaggship' target="_blank">
-        //                     <i className="fab fa-github-square"></i>
-        //                 </a>
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/montgomeryflagg/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
-        //             </div>
-        //         </div>
-        //         <div className='footer__name-link'>
-        //             <h3 className='footer__name'>Pierre Guilbault</h3>
-        //             <div className='footer__icons'>
-        //                 <a className='footer__link' href='https://github.com/TheGuilbotine' target="_blank">
-        //                     <i className="fab fa-github-square"></i>
-        //                 </a>
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/pierre-guilbault-30020549/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
-        //             </div>
-        //         </div>
-        //         <div className='footer__name-link'>
-        //             <h3 className='footer__name'>Sunny Mallick</h3>
-        //             <div className='footer__icons'>
-        //                 <a className='footer__link' href='https://github.com/sunnymallick' target="_blank">
-        //                     <i className="fab fa-github-square"></i>
-        //                 </a>
-        //                 <a className='footer__link' href='https://www.linkedin.com/in/sunny-mallick-896a33169/' target="_blank">
-        //                     <i className="fab fa-linkedin"></i>
-        //                 </a>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </footer>
     )
 }
