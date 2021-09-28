2727e9516944193d8b3e7c427579f2437463275b
Sunny Mallick <sunny.mallick927@gmail.com>
2021-09-28 16:19:07 -0600

    finished review state

--- react-app/src/store/reviews.js
+++ react-app/src/store/reviews.js
@@ -1,15 +1,15 @@
-export const LOAD_REVIEWS = 'comments/LOAD_REVIEWS';
-export const CREATE_REVIEW = 'comments/CREATE_REVIEW';
-export const DESTROY_REVIEW = 'comments/DESTROY_REVIEW';
+export const LOAD_REVIEWS = 'reviews/LOAD_REVIEWS';
+export const CREATE_REVIEW = 'reviews/CREATE_REVIEW';
+export const DESTROY_REVIEW = 'reviews/DESTROY_REVIEW';
 
 const load = reviews => ({
     type: LOAD_REVIEWS,
     reviews
 })
 
-const placeReview = comment => ({
+const placeReview = review => ({
     type: CREATE_REVIEW,
-    comment
+    review
 })
 
 const destroy = reviewId => ({
@@ -27,14 +27,14 @@ export const getReviews = () => async dispatch => {
     }
 }
 
-export const getOneReview = (id) => async dispatch => {
-    const res = await fetch(`/api/reviews/${id}`);
+// export const getOneReview = (id) => async dispatch => {
+//     const res = await fetch(`/api/reviews/${id}`);
 
-    const review = await res.json();
-    if (res.ok) {
-        dispatch(placeComment(review))
-    }
-}
+//     const review = await res.json();
+//     if (res.ok) {
+//         dispatch(placeReview(review))
+//     }
+// }
 
 export const createReview = (review, userId, photoUrl) => async dispatch => {
     const res = await fetch('/api/reviews/', {
@@ -48,7 +48,68 @@ export const createReview = (review, userId, photoUrl) => async dispatch => {
     })
     if (res.ok) {
         const newReview = await res.json();
-        dispatch(placeComment(newReview))
+        dispatch(placeReview(newReview))
         return newReview;
     }
 }
+
+export const editReview = (reviewId, review, userId, restaurantId) => async dispatch => {
+    const res = await fetch(`/api/reviews/${reviewId}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            review: review,
+            user_id: userId,
+            restaurant_id: restaurantId,
+        })
+    })
+
+    if (res.ok) {
+        const editedReview = await res.json();
+        dispatch(placeReview(editedReview))
+    }
+}
+
+export const deleteReview = (id) => async dispatch => {
+    const res = await fetch(`/api/reviews/${id}`, {
+        method: 'DELETE'
+    })
+    if (res.ok) {
+        await res.json();
+        dispatch(destroy(id))
+    }
+    return res;
+}
+
+
+let initialState = {}
+
+const reviewReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case LOAD_REVIEWS: {
+            const allReviews = {
+                ...state,
+            };
+            action.reviews.forEach((review) => {
+                allReviews[review.id] = review;
+            })
+            return allReviews
+        }
+        case CREATE_REVIEW: {
+            const newState = {
+                ...state,
+                [action.review?.id]: action.review
+            }
+            return newState
+        }
+        case DESTROY_REVIEW: {
+            const newState = {...state};
+                delete newState[action.reviewId]
+            return newState;
+        }
+        default:
+            return state
+    }
+}
+
+export default reviewReducer
