5650f77be7ecad35acce890ecfa5be7a1479f301
Monte <36746765+theflaggship@users.noreply.github.com>
2021-09-30 09:48:36 -0700

    google map debugging

--- react-app/src/components/GoogleMap/index.js
+++ react-app/src/components/GoogleMap/index.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import Geocode from "react-geocode";
 
-const MapContainer = ({addressLineOne, zipCode}) => {
+const MapContainer = async ({addressLineOne, zipCode}) => {
+
+console.log('------------------------------------');
+console.log("AddressLineOne", addressLineOne);
+console.log("Zip", zipCode);
+console.log('------------------------------------');
 
 Geocode.setApiKey("AIzaSyADWdTTdivnw39tHOT9H59yhbklqmON6T8");
+const address = addressLineOne + " " + zipCode
+let res = await Geocode.fromAddress(address)
 
-Geocode.setLanguage("en");
+// Geocode.setLanguage("en");
 
-Geocode.setRegion("us");
-let lat;
-let lng;
+// Geocode.setRegion("us");
+// let lat;
+// let lng;
 
-console.log('------------------------------------');
-console.log(Geocode.fromAddress(`${addressLineOne}, ${zipCode}`).then(
-    (response) => {
-      let { lat, lng } = response.results[0].geometry.location;
-      console.log('------------------------------------');
-      console.log(lat, lng);
-      console.log('------------------------------------');
-    },
-    (error) => {
-      console.error(error);
-    }
-));
-console.log('------------------------------------');
+// console.log('------------------------------------');
+// console.log(Geocode.fromAddress(`${addressLineOne}, ${zipCode}`).then(
+//     (response) => {
+//       let { lat, lng } = response.results[0].geometry.location;
+//       console.log('------------------------------------');
+//       console.log(lat, lng);
+//       console.log('------------------------------------');
+//     },
+//     (error) => {
+//       console.error(error);
+//     }
+// ));
+// console.log('------------------------------------');
 
   const mapStyles = {
     height: "100%",
